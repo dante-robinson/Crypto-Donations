bae27c691f8a85204cb23691f50ee69dd58986e2
dante-robinson <danterobinson2000@gmail.com>
2022-06-09 15:46:03 -0400

    Fix Footer not dynamically moving to bottom of the page.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,16 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
-    <div className="flex mt-8 h-[5vh] bg-crayola-blue items-center justify-end">
-      <a
-        href="https://github.com/dante-robinson/Unstoppable-Donations"
-        className="text-greyish-white"
-      >
-        Check out the project on Github
-      </a>
-      <FontAwesomeIcon className="text-greyish-white ml-2" icon={faGithub} />
+    <div className="flex pt-16">
+      <div className="fixed flex inset-x-0 bottom-0 mt-8 h-[5vh] bg-crayola-blue items-center justify-end">
+        <a
+          href="https://github.com/dante-robinson/Unstoppable-Donations"
+          className="text-greyish-white"
+        >
+          Check out the project on Github
+        </a>
+        <FontAwesomeIcon className="text-greyish-white ml-2" icon={faGithub} />
+      </div>
     </div>
   );
 };
